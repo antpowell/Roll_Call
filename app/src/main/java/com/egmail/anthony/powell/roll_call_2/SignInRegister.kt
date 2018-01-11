7245b9f96c9cc002041a7b0766be16b0d14734a9
antpowell <powell.anthony.e@gmail.com>
2018-01-11 17:25:31 -0600

    clean up Manifest file and rename MainActivity

--- app/src/main/java/com/egmail/anthony/powell/roll_call_2/SignInRegister.kt
+++ app/src/main/java/com/egmail/anthony/powell/roll_call_2/SignInRegister.kt
@@ -3,10 +3,10 @@ package com.egmail.anthony.powell.roll_call_2
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SignInRegister : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_in_register)
     }
 }
