ccc2d2c2e6da3a43d53591e288817559ba423e88
antpowell <powell.anthony.e@gmail.com>
2018-01-12 15:03:33 -0600

    added edittext to layout and fixed background image positioning issue

--- app/src/main/java/com/egmail/anthony/powell/roll_call_2/SignInRegister.kt
+++ app/src/main/java/com/egmail/anthony/powell/roll_call_2/SignInRegister.kt
@@ -1,12 +1,17 @@
 package com.egmail.anthony.powell.roll_call_2
 
+import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
 
 class SignInRegister : AppCompatActivity() {
-
+    companion object {
+        val displayMetric = DisplayMetrics()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in_register)
+
     }
 }
